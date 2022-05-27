5dc12c59e5c55c850e3bdaa1bb89f55ebfd49fe6
charlascuen <carlos.cuencae@alumnos.upm.es>
2022-05-27 09:33:59 +0200

    add settings

---
+++ settings/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
+let settings = null;
+
+async function saveSettings() {
+	await fsPromises.writeFile('./settings/config.json', JSON.stringify(settings));
+}
+
+async function setSettings(_settings) {
+	settings = _settings;
+	await saveSettings();
+}
+
+async function getSettings() {
+	if (!settings) {
+		if (fs.existsSync('./settings/config.json')) {
+			settings = JSON.parse(await fsPromises.readFile('./settings/config.json'));
+		} else {
+			settings = {
+				adminPassword: 'ContraseñaAdmin',
+			};
+		}
+		await saveSettings();
+	}
+
+	return settings;
+}
+module.exports = { setSettings, getSettings };
